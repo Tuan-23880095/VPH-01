2cfb97eab31050298d4b9a966a973135daf11b5a
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-03 15:46:22 +0700

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Thay đổi './' thành tên repository của bạn trên GitHub
   base: '/VPH-01/', 
 })
