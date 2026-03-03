cbcaedbaa01eb4e75d29ca718ebee512a1ce35ba
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-03 13:52:06 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Thay đổi dòng này thành './' để GitHub tự động nhận diện đúng thư mục (chống lỗi trắng trang)
+  // SỬA TẠI ĐÂY: Dùng './' thay vì '/VPH-01/' để web tự động nhận diện đúng thư mục
   base: './', 
 })
