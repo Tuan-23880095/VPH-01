a72afec0681913422cdef6d0bbbd3a1279a415ed
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-03 15:58:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // SỬA TẠI ĐÂY: Dùng './' thay vì '/VPH-01/' để web tự động nhận diện đúng thư mục
-  base: './', 
+  // Tên Repo của bạn là VPH-01, nên base path chuẩn xác nhất cho GitHub Pages là '/VPH-01/'
+  base: '/VPH-01/', 
 })
